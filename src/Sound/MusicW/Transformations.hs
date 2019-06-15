a318186cb1b4ff8157fec21244940385d1f32edf
d0kt0r0 <ogbornd@mcmaster.ca>
2019-06-15 18:03:04 -0400

    splay reworked so that stereo and multichannel have differet semantics

--- src/Sound/MusicW/Transformations.hs
+++ src/Sound/MusicW/Transformations.hs
@@ -46,11 +46,26 @@ circlePan n pos input = do
   xs <- mapM (\x -> gain x input) gs'
   channelMerger xs
 
+-- | splay pans a list of signals by distributing them equally over an array
+-- of speakers/outputs. When there are more than 2 outputs (as indicated by first
+-- argument) the signals are equidistantly distributed around a "circle" (using
+-- circlePan, above) with the first signal in the list at position 0. When there
+-- are only 2 outputs, equalPowerPan (above) is used instead, and the signals are
+-- panned such that a single signal will be centre panned, two signals will be
+-- panned hard left and right, and two or more signals will be distributed so that
+-- the first signal is guaranteed to be hard left and the last signal is hard right.
+
 splay :: AudioIO m => Int -> [NodeRef] -> SynthDef m NodeRef
 splay n [] = constantSource 0 >>= circlePan n 0
+splay 2 (i:[]) = equalPowerPan 0.5 i
 splay n (i:[]) = circlePan n 0.5 i
-splay n inputs = do
+splay 2 inputs = do
   let i = 1/(fromIntegral $ length inputs - 1)
+  let positions = fmap ((*) i) $ take (length inputs) [0..]
+  xs <- zipWithM equalPowerPan positions inputs
+  mix xs
+splay n inputs = do
+  let i = 1/(fromIntegral $ length inputs)
   let positions = fmap ((*) i) $ take (length inputs) [0..]
   xs <- zipWithM (circlePan n) positions inputs
   mix xs
